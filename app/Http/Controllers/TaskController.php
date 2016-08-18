1b17ab2b83f910976e1462e26efb83259058e684
ymgn <gonyamagon@gmail.com>
2016-08-18 18:37:52 +0900

    その他追加

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -68,7 +68,7 @@ class TaskController extends Controller
     public function destroy(Request $request,Task $task)
     {
         $this->authorize('destroy',$task);
-
+        
         $task->delete();
 
         return redirect('/tasks');
